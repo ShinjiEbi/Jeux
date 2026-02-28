1a0154ddf0cbab839370be592bec790a39482c56
ShinjiEbi <155539508+ShinjiEbi@users.noreply.github.com>
2026-02-28 20:48:36 +0100

    Upgrade service worker cache version and files

--- Jeu/neo-runner/sw.js
+++ Jeu/neo-runner/sw.js
@@ -1,15 +1,32 @@
-const CACHE = 'neo-runner-v2';
-const FILES = ['./Jeu/neo-runner/index.html','./Jeu/neo-runner/manifest.json'];
+const CACHE = 'neo-runner-v3';
+
+const FILES = [
+  './',
+  './index.html',
+  './manifest.json',
+  './nr_icon.svg'
+];
+
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
   self.skipWaiting();
 });
+
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))
-  ));
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+      )
+    )
+  );
   self.clients.claim();
 });
+
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
 });
