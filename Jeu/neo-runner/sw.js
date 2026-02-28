88d22d5f9d6cc0874e90a77a5235bb7b62254e99
ShinjiEbi <155539508+ShinjiEbi@users.noreply.github.com>
2026-02-28 20:29:03 +0100

    Update cache name and HTML file in service worker

--- Jeu/neo-runner/sw.js
+++ Jeu/neo-runner/sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'neon-runner-v1';
-const FILES = ['./neon_runner.html','./manifest.json'];
+const CACHE = 'neo-runner-v1';
+const FILES = ['./index.html','./manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
   self.skipWaiting();
